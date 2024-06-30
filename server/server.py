6b47fb6f751a3d7cb3d035b6128847c79569464c
CodeRyzen <73425895+CodeRyzen@users.noreply.github.com>
2024-06-30 21:08:51 +0400

    Add files via upload

---
+++ server/server.py
@@ -0,0 +1,15 @@
+import socket
+
+server = socket.socket()
+server.bind(('127.0.0.1', 8000))
+server.listen()
+print("Сервер запущен")
+
+while True:
+    client_socket, add = server.accept()
+    data = client_socket.recv(1024).decode('utf-8')
+    print(data)
+    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    content = "Hello!".encode('utf-8')
+    client_socket.send(HDRS.encode('utf-8') + content)
+    client_socket.shutdown(socket.SHUT_WR)
